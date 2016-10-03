486f1537e2a5b1499c8c31d3f886a9f948733f4f
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-03 15:33:27 +0100

    added balance of 0

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,6 @@
 class Oystercard
+  def initialize
+    @balance = 0
+  end
+  attr_reader :balance
 end
--- spec/oystercar_spec.rb
+++ spec/oystercar_spec.rb
@@ -1,4 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:card) { described_class.new }
+  it 'should have a balance of 0' do
+    expect(card.balance).to eq (0r)
+  end
 end
